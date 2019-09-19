bfa4b33615bb103c58d4960b3d359730770201d8
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-09-19 13:24:59 -0700

    Added base test class

--- src/test/java/com/newtours/tests/BookFlightTest.java
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -1,28 +1,22 @@
 package com.newtours.tests;
 
 import com.newtours.pages.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.tests.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class BookFlightTest {
+public class BookFlightTest extends BaseTest {
 
-    private WebDriver driver;
-    private String noOfPassangers;
+    private String noOfPassengers;
     private String expectedPrice;
 
     @BeforeTest
-    @Parameters({"noOfPassangers", "expectedPrice"})
-    public void setUpDriver(String noOfPassangers, String expectedPrice) {
-        this.noOfPassangers = noOfPassangers;
+    @Parameters({"noOfPassengers", "expectedPrice"})
+    public void setupParameters(String noOfPassengers, String expectedPrice){
+        this.noOfPassengers = noOfPassengers;
         this.expectedPrice = expectedPrice;
-        //set path
-        System.setProperty("webdriver.chrome.driver", "/Users/singh2_rajiv/Automation/Selenium/Drivers/chromedriver");
-        this.driver = new ChromeDriver();
     }
 
     @Test
@@ -35,34 +29,30 @@ public class BookFlightTest {
     }
 
     @Test(dependsOnMethods = "registrationPage")
-    public void registrationConfirmationPage() {
+    public void registrationConfirmationPage(){
         RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
-        registrationConfirmationPage.gotoFlightDetailsPage();
+        registrationConfirmationPage.goToFlightDetailsPage();
     }
 
     @Test(dependsOnMethods = "registrationConfirmationPage")
-    public void flightDetailsPage() {
+    public void flightDetailsPage(){
         FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
-        flightDetailsPage.selectPassengers(noOfPassangers);
+        flightDetailsPage.selectPassengers(noOfPassengers);
         flightDetailsPage.goToFindFlightsPage();
     }
 
     @Test(dependsOnMethods = "flightDetailsPage")
-    public void findFlightsPage() {
+    public void findFlightPage(){
         FindFlightPage findFlightPage = new FindFlightPage(driver);
         findFlightPage.submitFindFlightPage();
         findFlightPage.goToFlightConfirmationPage();
     }
 
-    @Test(dependsOnMethods = "findFlightsPage")
-    public void flightConfirmation() {
+    @Test(dependsOnMethods = "findFlightPage")
+    public void flightConfirmationPage(){
         FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
         String actualPrice = flightConfirmationPage.getPrice();
         Assert.assertEquals(actualPrice, expectedPrice);
     }
 
-    @AfterTest
-    public void quitBrowser() {
-        this.driver.quit();
-    }
 }
--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -1,37 +1,23 @@
 package com.searchmodule.tests;
 
-import com.newtours.pages.FlightConfirmationPage;
 import com.searchmodule.pages.SearchPage;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.tests.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class SearchTest {
-
-    private WebDriver driver;
-
-    @BeforeTest
-    public void setUpDriver() {
-        //set path
-        System.setProperty("webdriver.chrome.driver", "/Users/singh2_rajiv/Automation/Selenium/Drivers/chromedriver");
-        this.driver = new ChromeDriver();
-    }
+public class SearchTest extends BaseTest {
 
     @Test
-    public void search() throws InterruptedException{
+    @Parameters({"keyword"})
+    public void search(String keyword){
         SearchPage searchPage = new SearchPage(driver);
         searchPage.goTo();
-        searchPage.doSearch("java");
+        searchPage.doSearch(keyword);
         searchPage.goToVideos();
-        searchPage.getResult();
+        int size = searchPage.getResult();
+
+        Assert.assertTrue(size > 0);
     }
 
-    @AfterTest
-    public void quitBrowser() {
-        this.driver.quit();
-    }
 }
